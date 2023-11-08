3f6ca14fd32988bc8df6d62f7f714d7ad2a3843f
Loai Dev <loaidev64@gmail.com>
2023-11-08 10:23:08 +0200

    Added some extra guides to initialize localization

--- hooks/post_gen.dart
+++ hooks/post_gen.dart
@@ -2,7 +2,7 @@ import 'package:mason/mason.dart';
 
 void run(HookContext context) {
   context.logger
-      .warn('First: You need to add the following to your pubspec.yaml:');
+      .warn('First: You need to add the following to your pubspec.yaml file:');
   context.logger.info('''
 dependencies:
   flutter_localizations:
@@ -14,4 +14,19 @@ flutter:
 
   context.logger.warn(
       'Second: You need to wrap your MaterialApp/CupertinoApp widget with LocaizationBlocWidget');
+
+  context.logger
+      .warn('Third: You need to add the following to your main.dart file:');
+
+  context.logger.info('''
+          return MaterialApp(
+            supportedLocales: AppTranslation.all,
+            localizationsDelegates: const [
+              AppLocalizations.delegate,
+              GlobalCupertinoLocalizations.delegate,
+              GlobalMaterialLocalizations.delegate,
+              GlobalWidgetsLocalizations.delegate,
+            ],
+          );
+          ''');
 }
